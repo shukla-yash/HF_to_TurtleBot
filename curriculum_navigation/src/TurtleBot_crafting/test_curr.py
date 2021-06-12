9fdb77bceb0b7f129a02315de6eec475a0bf8306
Chris Thierauf <christopher.thierauf@tufts.edu>
2021-06-11 23:22:27 -0400

    formatting tweaks: black formatting

--- curriculum_navigation/src/TurtleBot_crafting/test_curr.py
+++ curriculum_navigation/src/TurtleBot_crafting/test_curr.py
@@ -11,6 +11,7 @@ import rospy
 from geometry_msgs.msg import Twist
 from movement_utils.srv import *
 
+
 def CheckTrainingDoneCallback(reward_array, done_array, env):
     done_cond = False
     reward_cond = False
@@ -34,20 +35,21 @@ def CheckTrainingDoneCallback(reward_array, done_array, env):
     else:
         return 0
 
-class GoForward():
+
+class GoForward:
     def __init__(self, action):
         # initiliaze
-        rospy.init_node('GoForward', anonymous=False)
+        rospy.init_node("GoForward", anonymous=False)
 
         # What function to call when you ctrl + c
         rospy.on_shutdown(self.shutdown)
 
         # Create a publisher which can "talk" to TurtleBot and tell it to move
         # Tip: You may need to change cmd_vel_mux/input/navi to /cmd_vel if you're not using TurtleBot2
-        self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+        self.cmd_vel = rospy.Publisher("cmd_vel_mux/input/navi", Twist, queue_size=10)
 
-        #TurtleBot will stop if we don't keep telling it to move.  How often should we tell it to move? 10 HZ
-        r = rospy.Rate(10);
+        # TurtleBot will stop if we don't keep telling it to move.  How often should we tell it to move? 10 HZ
+        r = rospy.Rate(10)
 
         # Twist is a datatype for velocity
         move_cmd = Twist()
@@ -71,17 +73,18 @@ class GoForward():
         rospy.loginfo("Stop TurtleBot")
         self.cmd_vel.publish(Twist())
 
+
 def main():
     no_of_environmets = 4
 
-    width_array = [1.5,2.5,3,3]
-    height_array = [1.5,2.5,3,3]
-    no_trees_array = [1,1,3,4]
-    no_rocks_array = [0,1,2,2]
-    crafting_table_array = [0,0,1,1]
-    starting_trees_array = [0,0,0,0]
-    starting_rocks_array = [0,0,0,0]
-    type_of_env_array = [0,1,2,2]
+    width_array = [1.5, 2.5, 3, 3]
+    height_array = [1.5, 2.5, 3, 3]
+    no_trees_array = [1, 1, 3, 4]
+    no_rocks_array = [0, 1, 2, 2]
+    crafting_table_array = [0, 0, 1, 1]
+    starting_trees_array = [0, 0, 0, 0]
+    starting_rocks_array = [0, 0, 0, 0]
+    type_of_env_array = [0, 1, 2, 2]
 
     total_timesteps_array = []
     total_reward_array = []
@@ -92,7 +95,7 @@ def main():
     task_completion_array = []
 
     actionCnt = 5
-    D = 83 #90 beams x 4 items lidar + 3 inventory items
+    D = 83  # 90 beams x 4 items lidar + 3 inventory items
     NUM_HIDDEN = 16
     GAMMA = 0.995
     LEARNING_RATE = 1e-3
@@ -102,7 +105,7 @@ def main():
 
     # agent = SimpleDQN(actionCnt,D,NUM_HIDDEN,LEARNING_RATE,GAMMA,DECAY_RATE,MAX_EPSILON,random_seed)
     # agent.set_explore_epsilon(MAX_EPSILON)
-    action_space = ['W','A','D','U','C']
+    action_space = ["W", "A", "D", "U", "C"]
     total_episodes_arr = []
 
     for k in range(1):
@@ -121,22 +124,57 @@ def main():
         final_status = False
 
         if i == 0:
-            agent = SimpleDQN(actionCnt,D,NUM_HIDDEN,LEARNING_RATE,GAMMA,DECAY_RATE,MAX_EPSILON,random_seed)
+            agent = SimpleDQN(
+                actionCnt,
+                D,
+                NUM_HIDDEN,
+                LEARNING_RATE,
+                GAMMA,
+                DECAY_RATE,
+                MAX_EPSILON,
+                random_seed,
+            )
             agent.set_explore_epsilon(MAX_EPSILON)
         else:
-            agent = SimpleDQN(actionCnt,D,NUM_HIDDEN,LEARNING_RATE,GAMMA,DECAY_RATE,MAX_EPSILON,random_seed)
+            agent = SimpleDQN(
+                actionCnt,
+                D,
+                NUM_HIDDEN,
+                LEARNING_RATE,
+                GAMMA,
+                DECAY_RATE,
+                MAX_EPSILON,
+                random_seed,
+            )
             agent.set_explore_epsilon(MAX_EPSILON)
-            agent.load_model(0,0,i)
+            agent.load_model(0, 0, i)
             agent.reset()
             print("loaded model")
 
-
-        if i == no_of_environmets-1:
+        if i == no_of_environmets - 1:
             final_status = True
 
-        env_id = 'TurtleBot-v0'
-        env = gym.make(env_id, map_width = width, map_height = height, items_quantity = {'tree': no_trees, 'rock': no_rocks, 'crafting_table': crafting_table, 'pogo_stick':0},
-            initial_inventory = {'wall': 0, 'tree': starting_trees, 'rock': starting_rocks, 'crafting_table': 0, 'pogo_stick':0}, goal_env = type_of_env, is_final = final_status)
+        env_id = "TurtleBot-v0"
+        env = gym.make(
+            env_id,
+            map_width=width,
+            map_height=height,
+            items_quantity={
+                "tree": no_trees,
+                "rock": no_rocks,
+                "crafting_table": crafting_table,
+                "pogo_stick": 0,
+            },
+            initial_inventory={
+                "wall": 0,
+                "tree": starting_trees,
+                "rock": starting_rocks,
+                "crafting_table": 0,
+                "pogo_stick": 0,
+            },
+            goal_env=type_of_env,
+            is_final=final_status,
+        )
 
         t_step = 0
         episode = 0
@@ -155,7 +193,7 @@ def main():
             obs = env.get_observation()
 
             # act
-            a = agent.process_step(obs,True)
+            a = agent.process_step(obs, True)
             if a == 0:
                 print("Right")
                 GoForward(0)
@@ -188,7 +226,13 @@ def main():
                     done_arr.append(0)
                     task_completion_array.append(0)
 
-                print("\n\nfinished episode = "+str(episode)+" with " +str(reward_sum)+"\n")
+                print(
+                    "\n\nfinished episode = "
+                    + str(episode)
+                    + " with "
+                    + str(reward_sum)
+                    + "\n"
+                )
 
                 reward_arr.append(reward_sum)
                 avg_reward.append(np.mean(reward_arr[-40:]))
@@ -212,7 +256,7 @@ def main():
                 reward_sum = 0
 
                 env_flag = 0
-                if i< 3:
+                if i < 3:
                     env_flag = CheckTrainingDoneCallback(reward_arr, done_arr, i)
 
                 # quit after some number of episodes
@@ -220,6 +264,7 @@ def main():
 
                     break
     print("done")
+
 
 if __name__ == "__main__":
     main()
